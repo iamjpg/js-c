0a8bc6162c720c8b33bdf7be6226e4c4ba0cb60b
JP Given <jgiven@gmail.com>
2021-08-19 15:39:03 -0700

    A little more layout has been handled.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { useEffect } from 'react';
 import cn from 'classnames';
 import { CoinContainer } from './components/CoinContainer';
 import { LandingPad } from './components/LandingPad';
 import { mainContainer } from './css-modules/Layout.module.css';
 
+// https://api.coingecko.com/api/v3/coins/dogecoin
+
 function App() {
+  const testFetch = () => {
+    fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (err) {
+        console.warn('Something went wrong.', err);
+      });
+  };
+
+  useEffect(() => {
+    // testFetch();
+  }, []);
+
   return (
     <>
       <div className={cn('container', mainContainer)}>
         <div className='row'>
+          <h1>Bitcoin Drag n' Drop</h1>
+          <p>
+            Step right up and drag a crypto-currency to the landing pad to
+            return data!
+          </p>
           <div className='twelve columns'>
             <CoinContainer />
             <LandingPad />
--- src/components/CoinContainer.js
+++ src/components/CoinContainer.js
@@ -1,7 +1,21 @@
+import { container } from '../css-modules/CoinContainer.module.css';
+
+import BitcoinLogo from '../static/svg/btc.png';
+import EthereumLogo from '../static/svg/eth.png';
+import StellarLogo from '../static/svg/xlm.png';
+import XrpLogo from '../static/svg/xrp.png';
+import DogeLogo from '../static/svg/doge.png';
+
 export const CoinContainer = () => {
   return (
     <>
-      <div>Coins</div>
+      <div className={container}>
+        <img src={BitcoinLogo} width={50} alt='Bitcoin Logo' />
+        <img src={EthereumLogo} width={50} alt='Eth Logo' />
+        <img src={StellarLogo} width={50} alt='Stellar Logo' />
+        <img src={XrpLogo} width={50} alt='Ripple Logo' />
+        <img src={DogeLogo} width={50} alt='Dogecoin Logo' />
+      </div>
     </>
   );
 };
